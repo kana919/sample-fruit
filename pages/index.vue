eefb261c30232c5208b436e31563c818982c6a79
kana919 <kaitakana.3093@gmail.com>
2020-07-25 19:35:44 +0900

    フルーツ画像追加。アンカー押下関数追加。

--- pages/index.vue
+++ pages/index.vue
@@ -4,13 +4,13 @@
       <h1 class="title">フルーツを選んでね</h1>
     </div>
     <div class="button-box">
-      <a href="#" v-on:click="getFruit" class="button--grey">もも</a>
-      <a href="#" v-on:click="getFruit" class="button--grey">りんご</a>
-      <a href="#" v-on:click="getFruit" class="button--grey">すいか</a>
-      <a href="#" v-on:click="getFruit" class="button--grey">みかん</a>
+      <a href="#" v-on:click="getPeach" class="button--grey">もも</a>
+      <a href="#" v-on:click="getApple" class="button--grey">りんご</a>
+      <a href="#" v-on:click="getSuika" class="button--grey">すいか</a>
+      <a href="#" v-on:click="getMikan" class="button--grey">みかん</a>
     </div>
     <div class="illust-box">
-      <img v-bind:src="imgPath"/>
+      <img v-bind:src="dispImg"/>
     </div>
   </section>
 </template>
@@ -19,34 +19,47 @@
 export default
 {
   data: () => ({
-    imgPath: ""
+    dispImg: ''
   }),
 
   methods: {
-    getFruit() {
-      this.imgPath = require('@/assets/peach.png');
-    }
+    getPeach() {
+      this.dispImg = require('@/assets/peach.png');
+    },
+    getApple() {
+      this.dispImg = require('@/assets/apple.png');
+    },
+    getSuika(){
+      this.dispImg = require('@/assets/suika.png');
+    },
+    getMikan(){
+      this.dispImg = require('@/assets/mikan.png');
+    },
   }
 }
 </script>
 
 <style>
-
 .container {
   text-align:center;
   margin-top:10px;
 }
 
 .title{
-  color:#4682b4;
+  color:#FFC0CB;
 }
 
-.button-box{
-  margin-top:30px;
-}
-
+.button-box,
 .illust-box{
   margin-top:30px;
 }
 
+.button-box a{
+  border-color:#FFA07A;
+  color:#FFA07A;
+}
+
+.button-box a:hover{
+  background-color: #FFA07A;
+}
 </style>
